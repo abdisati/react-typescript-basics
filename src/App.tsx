c4d188f4635c44ea3bce2284b06b5cc5b5d9ebfe
Abdi Deresa <Abdishegute@gmail.com>
2025-01-22 08:22:26 -0800

    feat: enhance CourseGoal component to accept title and description as props

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import CourseGoal from "./components/CourseGoal.tsx";
 export default function App() {
   return (
     <main>
-      <CourseGoal />
+      <CourseGoal
+        title="Learn React + Ts"
+        description="Learn it from the ground up"
+      />
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,15 @@
-export default function CourseGoal(){
-    return (
-        <article>
-            <div>
-                <h2>TITLE</h2>
-                <p>DESCRIPTION</p>
-            </div>
-            <button>Delete</button>
-        </article>
-    )
+export default function CourseGoal(props: {
+  title: string;
+  description: string;
+}): JSX.Element {
+  const { title, description } = props;
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
 }
