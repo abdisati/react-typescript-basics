6eab50ef42ae6caf0e21125d01d99835735411e6
Abdi Deresa <Abdishegute@gmail.com>
2025-01-22 12:48:06 -0800

    feat: implement goal management in App with dynamic addition of CourseGoal items

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import CourseGoal from "./components/CourseGoal.tsx";
 import goalsImage from "./assets/goals.jpg";
 import Header from "./components/Header.tsx";
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  interface CourseGoal {
+    title: string;
+    description: string;
+    id: number;
+  }
+
+  const handleAddGoal = () => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        title: "Learn React + Ts",
+        id: Math.random(),
+        description: "Learn it in depth!",
+      };
+      return [...prevGoals, newGoal];
+    });
+  };
+
   return (
     <main>
       <Header image={{ src: goalsImage, alt: "A list of goals" }}>
-        <h1>Your Course Goals</h1>
+        <h1>My Learning Goals</h1>
       </Header>
-      <CourseGoal title="Learn React + Ts">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add New Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>{goal.description}</CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
